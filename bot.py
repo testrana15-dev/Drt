22d37732276b5625abd8ca943029d5494d783fcc
testrana15-dev <testrana15@gmail.com>
2026-04-14 18:49:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,7 +50,7 @@ def start_health_server():
 
 # ============ CLIENT ============
 app = Client(
-    "yt_uploader_bot",
+    "railway_session",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
